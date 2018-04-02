7bc5e7c2d990220c1bf160bd760b0930c8140b0e
Hoang <hoangvl16384@gmail.com>
2018-04-02 22:36:05 +0800

    temp2

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,4 +1,73 @@
 package client;
 
+import java.net.SocketException;
+import java.util.HashMap;
+import java.util.Map;
+import java.net.DatagramPacket;
+import client.ReplyReceiver.Reply_Mode;
+
+import java.net.InetAddress;
+
+import shared.Constant;
+import shared.Request;
+import shared.SocketWrapper;
+
 public class Client {
+	private InetAddress serverIp;
+	private int serverPort;
+    private SocketWrapper socket;
+
+    public Client(InetAddress serverIp, int serverPort){
+    	this.serverIp = serverIp;
+        this.serverPort = serverPort;
+    }
+    public void start() throws SocketException {
+        socket = new SocketWrapper(serverPort);
+    }
+    
+    public void handleReply(Request reply){
+    	String id = reply.getType();
+    }
+    
+    public String run(){
+        while (true) {
+            DatagramPacket packet = socket.receivePacket();
+            String error = socket.getErrMsg();
+            if (error != null) {
+                /*if (error.equals(ClientSocket.TIMEOUT)) {
+                    System.out.println("Timeout receiving reply. Retransmit request...");
+                    clientSocket.sendRequest(request);
+                    continue;
+                }*/
+                return error;
+            }
+            Request reply = Request.unmarshal(packet.getData());
+            if (reply.statusCode == Reply.ERROR_REPLY_CODE) {
+                return reply.getErrorMessage();
+
+            } else {
+                callback.handle(reply.getPayloads());
+                if (replyReceiveMode == Reply_Mode.NORMAL) {
+                    break;
+                } else {
+                    while (true) {
+                        data = clientSocket.receiveReply();
+                        error = clientSocket.error();
+                        if (error != null) {
+                            if (!error.equals(ClientSocket.TIMEOUT)) {
+                                return error;
+                            }
+                            continue;
+                        }
+                        reply = Reply.unmarshal(data);
+                        callback.handle(reply.getPayloads());
+                        if (reply.getPayloads().get(1).equals(Constant.STOP_MONITOR)) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
